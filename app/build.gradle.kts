61464a48fccdb4ea3913a6990cbd3ac3a32ee25c
Paul <ddanss@gmail.com>
2025-06-24 16:32:56 -0700

    add play-integrity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,7 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
     implementation("com.google.firebase:firebase-ai")
+    implementation("com.google.firebase:firebase-appcheck-playintegrity")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/ddanss/airecipe/MainApplication.kt
+++ app/src/main/java/com/ddanss/airecipe/MainApplication.kt
@@ -3,6 +3,10 @@ package com.ddanss.airecipe
 import android.app.Application
 import androidx.room.Room
 import com.ddanss.airecipe.room.AppDatabase
+import com.google.firebase.appcheck.ktx.appCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 
 class MainApplication : Application() {
     val database: AppDatabase by lazy {
@@ -11,5 +15,13 @@ class MainApplication : Application() {
             AppDatabase::class.java,
             "database-airecipe"
         ).build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Firebase.initialize(context = this)
+        Firebase.appCheck.installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance(),
+        )
     }
 }
